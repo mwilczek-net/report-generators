5ad294e889a95bbf95d4ade9cda79ab838e09c94
mwilczek-net <mwilczek-net@users.noreply.github.com>
2024-03-28 15:19:09 +0100

    List templates form selected directory

--- out-off-the-office-messages/generate.sh
+++ out-off-the-office-messages/generate.sh
@@ -5,7 +5,7 @@ CURRENT_PATH="$(cd "$(dirname "$0")" && pwd -P)"
 TEMPLATES="${CURRENT_PATH}/templates"
 
 function read_date {
-    # local date_value
+    local date_value
     date_value=`dialog --stdout --title "OUT OF THE OFFICE" --date-format "%Y-%m-%d" --calendar "Please choose ${2}" 0 0`
     eval "$1=${date_value}"
 }
@@ -13,8 +13,7 @@ function read_date {
 function clear_terminal {
     printf '\033[2J'
 }
-
-function print_summary {
+function print_dates_summary {
 cat <<EOF
 Generate Out Of The Office message:
 Start Date: ${date_from}
@@ -27,18 +26,51 @@ function confirm_generation {
     read
 }
 
+function list_templates_in_directory {
+    local selected_directory
+    local templates
+
+
+    selected_templates="${1}"
+    selected_directory="${TEMPLATES}/${selected_templates}"
+
+    echo "Generating results form templates \"${selected_templates}\""
+
+    pushd "${selected_directory}"
+    templates=(${(@f)"$(ls -1)"})
+    echo $templates
+    popd
+}
+
+
+function list_template_directories {
+    local templates
+    local templates_directories
+
+    pushd "${TEMPLATES}"
+    templates_directories=(${(@f)"$(ls -1)"})
+    popd
+
+    echo "Select templates directory"
+    echo "CTRL+D for exit"
+    select templates in $templates_directories; do
+        list_templates_in_directory "${templates}";
+    done
+}
+
+
 
 date_from=""
 date_to=""
 read_date date_from "START DATE"
 read_date date_to "END DATE"
 
-
 clear_terminal
-print_summary
+print_dates_summary
+list_template_directories
 confirm_generation
 
 
 
-# myarray=(${(@f)"$(ls -1)"})
-# select a in $myarray; do echo $a; done
+
+
