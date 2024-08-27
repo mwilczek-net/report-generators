9409301c636e89168d3f2a62050cff76a9cf9234
mwilczek-net <mwilczek-net@users.noreply.github.com>
2024-08-27 13:38:47 +0200

    Ask for homebrew

--- out-off-the-office-messages/requirements_macos_homebrew.sh
+++ out-off-the-office-messages/requirements_macos_homebrew.sh
@@ -1,5 +1,14 @@
 #!/bin/zsh
 
-brew --version || /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+function ask_for_homebrew {
+    echo "Missing hombebrew"
+    echo "Please isntall it"
+    echo ""
+    echo "https://brew.sh/"
+    echo ""
+    exit 1
+}
+
+brew --version || ask_for_homebrew
 
 brew install dialog
